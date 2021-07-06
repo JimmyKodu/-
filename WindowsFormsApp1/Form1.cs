152a15d897bc2bfc6cda644f6658e493cd5c4f98
jdsxk <jdsxk@DESKTOP-JL2LMGP>
2021-07-06 19:43:58 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.label6.Text = "3";
+            this.label5.Text = "5";
+            this.label4.Text = "7";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
