7c8e11a58d8852b0b8fd43eea9279f841e40479b
jdsxk <jdsxk@DESKTOP-JL2LMGP>
2021-07-07 20:23:09 +0800

    1.0

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,18 +33,18 @@ namespace WindowsFormsApp1
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
@@ -68,7 +68,7 @@ namespace WindowsFormsApp1
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(142, 13);
+            this.button2.Location = new System.Drawing.Point(182, -1);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -78,46 +78,35 @@ namespace WindowsFormsApp1
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(223, 13);
+            this.button3.Location = new System.Drawing.Point(182, 28);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
             this.button3.Text = "乙取";
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(304, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "1";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(304, 208);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 21);
-            this.textBox2.TabIndex = 8;
-            this.textBox2.Text = "1";
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(223, 207);
+            this.button4.Location = new System.Drawing.Point(182, 192);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 7;
-            this.button4.Text = "乙取";
+            this.button4.Text = "甲取";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(142, 207);
+            this.button5.Location = new System.Drawing.Point(182, 221);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 6;
-            this.button5.Text = "甲取";
+            this.button5.Text = "乙取";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // label2
             // 
@@ -128,31 +117,26 @@ namespace WindowsFormsApp1
             this.label2.TabIndex = 5;
             this.label2.Text = "第二行";
             // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(304, 417);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 21);
-            this.textBox3.TabIndex = 12;
-            this.textBox3.Text = "1";
-            // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(223, 416);
+            this.button6.Location = new System.Drawing.Point(182, 402);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 11;
-            this.button6.Text = "乙取";
+            this.button6.Text = "甲取";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(142, 416);
+            this.button7.Location = new System.Drawing.Point(182, 431);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(75, 23);
             this.button7.TabIndex = 10;
-            this.button7.Text = "甲取";
+            this.button7.Text = "乙取";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Visible = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // label3
             // 
@@ -166,16 +150,16 @@ namespace WindowsFormsApp1
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(410, 422);
+            this.label4.Location = new System.Drawing.Point(440, 19);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(11, 12);
             this.label4.TabIndex = 15;
-            this.label4.Text = "7";
+            this.label4.Text = "3";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(410, 213);
+            this.label5.Location = new System.Drawing.Point(440, 213);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(11, 12);
             this.label5.TabIndex = 14;
@@ -184,29 +168,77 @@ namespace WindowsFormsApp1
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(410, 19);
+            this.label6.Location = new System.Drawing.Point(440, 422);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(11, 12);
             this.label6.TabIndex = 13;
-            this.label6.Text = "3";
+            this.label6.Text = "7";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.comboBox1.Location = new System.Drawing.Point(304, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 16;
+            this.comboBox1.Text = "1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox2.Location = new System.Drawing.Point(304, 210);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 17;
+            this.comboBox2.Text = "1";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.comboBox3.Location = new System.Drawing.Point(304, 419);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 20);
+            this.comboBox3.TabIndex = 18;
+            this.comboBox3.Text = "1";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
@@ -224,18 +256,18 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,14 +19,189 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.label6.Text = "3";
+            this.label4.Text = "3";
+            this.button2.Visible = true;
+            this.button3.Visible = false;
             this.label5.Text = "5";
-            this.label4.Text = "7";
+            this.button4.Visible = true;
+            this.button5.Visible = false;
+            this.label6.Text = "7";
+            this.button6.Visible = true;
+            this.button7.Visible = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            this.label4.Text=(Convert.ToInt32(this.label4.Text.ToString())-Convert.ToInt32(this.comboBox1.Text.ToString())).ToString();
+            if(Convert.ToInt32(this.label4.Text.ToString()) == 0 && Convert.ToInt32(this.label5.Text.ToString()) == 0 && Convert.ToInt32(this.label6.Text.ToString()) == 0)
+            {
+                MessageBox.Show("甲输");
+            }
+            this.button2.Visible = false;
+            if (Convert.ToInt32(this.label4.Text.ToString()) > 0)
+            {
+                this.button3.Visible = true;
+            }
+            this.button4.Visible = false;
+            if (Convert.ToInt32(this.label5.Text.ToString()) > 0)
+            {
+                this.button5.Visible = true;
+            }
+            this.button6.Visible = false;
+            if (Convert.ToInt32(this.label6.Text.ToString()) > 0)
+            {
+                this.button7.Visible = true;
+            }
+            this.comboBox1.Text = "1";
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.label4.Text = (Convert.ToInt32(this.label4.Text.ToString()) - Convert.ToInt32(this.comboBox1.Text.ToString())).ToString();
+            if (Convert.ToInt32(this.label4.Text.ToString()) == 0 && Convert.ToInt32(this.label5.Text.ToString()) == 0 && Convert.ToInt32(this.label6.Text.ToString()) == 0)
+            {
+                MessageBox.Show("乙输");
+            }
+            if (Convert.ToInt32(this.label4.Text.ToString()) > 0)
+            {
+                this.button2.Visible = true;
+            }
+            this.button3.Visible = false;
+            if (Convert.ToInt32(this.label5.Text.ToString()) > 0)
+            {
+                this.button4.Visible = true;
+            }
+            this.button5.Visible = false;
+            if (Convert.ToInt32(this.label6.Text.ToString()) > 0)
+            {
+                this.button6.Visible = true;
+            }
+            this.button7.Visible = false;
+            this.comboBox1.Text = "1";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.label5.Text = (Convert.ToInt32(this.label5.Text.ToString()) - Convert.ToInt32(this.comboBox2.Text.ToString())).ToString();
+            if (Convert.ToInt32(this.label4.Text.ToString()) == 0 && Convert.ToInt32(this.label5.Text.ToString()) == 0 && Convert.ToInt32(this.label6.Text.ToString()) == 0)
+            {
+                MessageBox.Show("甲输");
+            }
+            this.button4.Visible = false;
+            if (Convert.ToInt32(this.label5.Text.ToString()) > 0)
+            {
+                this.button5.Visible = true;
+            }
+            this.button2.Visible = false;
+            if (Convert.ToInt32(this.label4.Text.ToString()) > 0)
+            {
+                this.button3.Visible = true;
+            }
+            this.button6.Visible = false;
+            if (Convert.ToInt32(this.label6.Text.ToString()) > 0)
+            {
+                this.button7.Visible = true;
+            }
+            this.comboBox2.Text = "1";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.label5.Text = (Convert.ToInt32(this.label5.Text.ToString()) - Convert.ToInt32(this.comboBox2.Text.ToString())).ToString();
+            if (Convert.ToInt32(this.label4.Text.ToString()) == 0 && Convert.ToInt32(this.label5.Text.ToString()) == 0 && Convert.ToInt32(this.label6.Text.ToString()) == 0)
+            {
+                MessageBox.Show("乙输");
+            }
+            if (Convert.ToInt32(this.label5.Text.ToString()) > 0)
+            {
+                this.button4.Visible = true;
+            }
+            this.button5.Visible = false;
+            if (Convert.ToInt32(this.label4.Text.ToString()) > 0)
+            {
+                this.button2.Visible = true;
+            }
+            this.button3.Visible = false;
+            if (Convert.ToInt32(this.label6.Text.ToString()) > 0)
+            {
+                this.button6.Visible = true;
+            }
+            this.button7.Visible = false;
+            this.comboBox2.Text = "1";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.label6.Text = (Convert.ToInt32(this.label6.Text.ToString()) - Convert.ToInt32(this.comboBox3.Text.ToString())).ToString();
+            if (Convert.ToInt32(this.label4.Text.ToString()) == 0 && Convert.ToInt32(this.label5.Text.ToString()) == 0 && Convert.ToInt32(this.label6.Text.ToString()) == 0)
+            {
+                MessageBox.Show("甲输");
+            }
+            this.button6.Visible = false;
+            if (Convert.ToInt32(this.label6.Text.ToString()) > 0)
+            { 
+                this.button7.Visible = true;
+            }
+            this.button2.Visible = false;
+            if (Convert.ToInt32(this.label4.Text.ToString()) > 0)
+            {
+                this.button3.Visible = true;
+            }
+            this.button4.Visible = false;
+            if (Convert.ToInt32(this.label5.Text.ToString()) > 0)
+            {
+                this.button5.Visible = true;
+            }
+            this.comboBox3.Text = "1";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.label6.Text = (Convert.ToInt32(this.label6.Text.ToString()) - Convert.ToInt32(this.comboBox3.Text.ToString())).ToString();
+            if (Convert.ToInt32(this.label4.Text.ToString()) == 0 && Convert.ToInt32(this.label5.Text.ToString()) == 0 && Convert.ToInt32(this.label6.Text.ToString()) == 0)
+            {
+                MessageBox.Show("乙输");
+            }
+            if (Convert.ToInt32(this.label6.Text.ToString()) > 0)
+            {
+                this.button6.Visible = true;
+            }
+            this.button7.Visible = false;
+            if (Convert.ToInt32(this.label4.Text.ToString()) > 0)
+            {
+                this.button2.Visible = true;
+            }
+            this.button3.Visible = false;
+            if (Convert.ToInt32(this.label5.Text.ToString()) > 0)
+            {
+                this.button4.Visible = true;
+            }
+            this.button5.Visible = false;
+            this.comboBox3.Text = "1";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(Convert.ToInt32(this.comboBox1.Text.ToString())> Convert.ToInt32(this.label4.Text.ToString()))
+            {
+                this.comboBox1.Text = "1";
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(this.comboBox2.Text.ToString()) > Convert.ToInt32(this.label5.Text.ToString()))
+            {
+                this.comboBox2.Text = "1";
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(this.comboBox3.Text.ToString()) > Convert.ToInt32(this.label6.Text.ToString()))
+            {
+                this.comboBox3.Text = "1";
+            }
         }
     }
 }
